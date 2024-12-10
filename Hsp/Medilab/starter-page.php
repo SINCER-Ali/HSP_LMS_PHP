8cc40f6bd47dc4fe7e2748d998b93f339954f902
Ilian lavoisne <lavoisne67@gmail.com>
2024-12-10 12:25:30 +0100

    fix: bouton forum etudiant

--- Hsp/Medilab/starter-page.php
+++ Hsp/Medilab/starter-page.php
@@ -78,7 +78,13 @@ $evenements = $evenement->getAllEvenements();
             <nav id="navmenu" class="navmenu">
                 <ul>
                     <li><a href="#hero">Accueil<br></a></li>
-                    <li><a href="etudiant_forum.php">Forum</a></li>
+                    <?php if(isset($_SESSION['role']) && $_SESSION['role'] == 1){ ?>
+                        <div class="text-center mt-4">
+                            <a href="etudiant_forum.php" class="btn btn-primary">
+                                <i class="bi bi-plus-circle"></i> Forum
+                            </a>
+                        </div>
+                    <?php } ?>
                     <li><a href="#about">À propos</a></li>
                     <li><a href="offres.php">Offres</a></li>
                     <li><a href="#departments">Départements</a></li>
